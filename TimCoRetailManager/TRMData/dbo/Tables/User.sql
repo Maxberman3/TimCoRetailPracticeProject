19db3d53258f53d94237a3f1b765f9393bbd52cc
Berman, Maxwell <maxwell.berman@emerson.com>
2020-04-13 15:20:02 -0400

    Finished the module on sql creation via the microsoft sql project

---
+++ TimCoRetailManager/TRMData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+( 
+    [Id] NVARCHAR(128) NOT NULL Primary Key, 
+    [FirstName] NCHAR(50) NOT NULL, 
+    [LastName] NCHAR(50) NOT NULL, 
+    [EmailAddress] NCHAR(256) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
