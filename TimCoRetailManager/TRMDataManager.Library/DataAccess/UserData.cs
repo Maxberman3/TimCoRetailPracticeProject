e79c47199eff9cb2c82524668c36df956bea37fc
Berman, Maxwell <maxwell.berman@emerson.com>
2020-04-16 17:17:47 -0400

    Finished the first part of data access module, need go back and debug the auth

---
+++ TimCoRetailManager/TRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+            var p = new { Id = id };
+            List<UserModel> output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "TRMData");
+            return output;
+        }
+    }
+}
---
+++ TimCoRetailManager/TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string id = RequestContext.Principal.Identity.GetUserId();
+            UserData userData = new UserData();
+            return userData.GetUserById(id);
+
+        }
+    }
+}
